e9ea4b5f849d915b0afb1cc8ff4f3680f2322ba1
vjeuel <vjeuel@gmail.com>
2020-11-18 12:16:26 -0600

    form changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,22 +5,15 @@ function Form() {
 	return (
 		<div className='formBox'>
 			<form>
-				<label>
-					First Name:
-					<input type='text' name='firstName' required />
-				</label>
-				<label>
-					Last Name:
-					<input type='text' name='lastName' required />
-				</label>
-				<label>
-					Phone Number:
-					<input type='tel' name='phone' /* pattern='[0-9]{3}[0-9]{3}[0-9]{4}'*/ required />
-				</label>
-				<label>
-					Address:
-					<input type='text' name='address' />
-				</label>
+				<input type='text' name='firstName' placeholder='First Name' required />
+				<input type='text' name='lastName' placeholder='Last Name' required />
+				<input
+					type='tel'
+					name='phone'
+					placeholder='Phone Number'
+					/* pattern='[0-9]{3}[0-9]{3}[0-9]{4}'*/ required
+				/>
+				<input type='text' name='address' placeholder='Address' />
 				<input className='button' type='submit' value='SUBMIT' />
 			</form>
 		</div>
